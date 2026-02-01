a8c71902dbd6e358b1ddbd87f0517f2e038557df
Sujan Poudel <147056997+sujanpoudelll@users.noreply.github.com>
2026-02-01 16:29:54 +0545

    Added search user functionality
    Implemented load_users() and search_users() with menu integration

--- user_management.py
+++ user_management.py
@@ -56,6 +56,51 @@ def view_users():
     except FileNotFoundError:
         print("No users or file found yet! Please add.")
 
+def load_users():
+    """Load all users from the file."""
+    users = []
+    try:
+        with open("user_management.txt","r") as file:
+            for line in file:
+                parts = line.strip().split(":")
+                if len(parts) != 3:
+                    continue
+
+                name, email, age = parts
+                users.append({
+                    "name":name,
+                    "email":email,
+                    "age":int(age),
+
+                })
+
+    except FileNotFoundError:
+        print("File Not Found !")
+    
+    return users
+
+def search_users():
+    """Load and display specific users by using name or email."""
+
+    users = load_users()
+    found = False
+    keyword = input("Enter name or email to search: ").lower().strip()
+    for user in users:
+        if keyword in user["name"].lower() or keyword in user["email"].lower():
+            print("USER FOUND !\n")
+            print(f"Name: {user['name']} \n")
+            print(f"Email: {user['email']} \n")
+            print(f"Age: {user['age']} \n")
+            found = True
+    if not found:
+        print("No Matched User Found !")
+            
+       
+
+
+
+
+
 
 def main_menu():       
 
@@ -67,22 +112,26 @@ def main_menu():
             print("#######USER MANAGEMENT SYSTEM########")
             print("1. ADD USERS")
             print("2. VIEW USERS")
-            print("3. EXIT")
+            print("3. SEARCH USER")
+            print("4. EXIT")
             
-            option = int(input("Select an option (1, 2, 3): "))
+            option = int(input("Select an option (1, 2, 3, 4): "))
 
             if option == 1:
                 add_users()
                 
             elif option == 2:
                 view_users()
-                
+            
             elif option == 3:
+                search_users()
+                
+            elif option == 4:
                 print("\nThank You !")
                 break
                 
             else:
-                print("Invalid option. Please choose 1, 2, or 3.")
+                print("Invalid option. Please choose 1, 2, 3, or 4.")
         except ValueError:
             print("Please enter a valid integer.")
         except Exception as e:

51c029034d497a2c0a9c9f62b69c7629ec7b655c
Sujan Poudel <147056997+sujanpoudelll@users.noreply.github.com>
2026-02-01 21:16:46 +0545

    Handle empty file case in view_users
    Added check for empty file before displaying users

--- user_management.py
+++ user_management.py
@@ -40,6 +40,11 @@ def view_users():
     try:
         with open("user_management.txt","r") as file:
             users = file.readlines()
+        
+
+        if not users:
+            print("No users found.")
+            return
 
        
 
@@ -83,21 +88,90 @@ def search_users():
     """Load and display specific users by using name or email."""
 
     users = load_users()
+
+    if not users:
+        print("No users found.")
+        return
+
+
     found = False
     keyword = input("Enter name or email to search: ").lower().strip()
     for user in users:
         if keyword in user["name"].lower() or keyword in user["email"].lower():
-            print("USER FOUND !\n")
+            print("\nUSER FOUND !\n")
             print(f"Name: {user['name']} \n")
             print(f"Email: {user['email']} \n")
             print(f"Age: {user['age']} \n")
             found = True
     if not found:
         print("No Matched User Found !")
+
+def update_users():
+    """Load and update users data and store ."""
+    users = load_users()
+
+    if not users:
+        print("No users found.")
+        return
+
+
+    
+
+    keyword = input("Enter name or email to update: ").lower().strip()
+    updated = False
+    for user in users:
+        if keyword in user["name"].lower() or keyword in user["email"].lower():
+            print("USER FOUND !\n")
+            print(f"Current Name: {user['name']} \n")
+            print(f"Current Email: {user['email']} \n")
+            print(f"Current Age: {user['age']} \n")
             
-       
+            new_name = input("Enter new name (leave blank to keep same): ")
+            new_email = input("Enter new email (leave blank to keep same): ")
+            while True:   
+                    
+                    new_age = (input("Enter age (leave blank to keep same): "))
 
+                    if new_age == "":
+                        new_age = user["age"]
+                        break
+                                       
+                    try:
+                        new_age = int(new_age)
 
+                        if new_age <= 0:
+                            print("Age can't be negative or zero !")
+
+                        elif new_age < 18:
+                            print("Age need to be atleast 18 !")
+                            
+                        else:
+                            break
+
+                    except ValueError:
+                        print("Age should be an integer !")
+            
+
+            if new_name:
+                user["name"] = new_name
+            if new_email: 
+                user["email"] = new_email
+            user["age"]= new_age
+            
+
+            updated = True
+            break
+
+    if not updated:
+        print("No matching user found.")
+    
+    else:
+            
+        with open("user_management.txt","w") as file:
+            for user in users:
+                file.write(f"{user['name']}:{user['email']}:{user['age']}\n")
+
+        print("User updated successfully!")
 
 
 
@@ -113,9 +187,10 @@ def main_menu():
             print("1. ADD USERS")
             print("2. VIEW USERS")
             print("3. SEARCH USER")
-            print("4. EXIT")
+            print("4. UPDATE USER")
+            print("5. EXIT")
             
-            option = int(input("Select an option (1, 2, 3, 4): "))
+            option = int(input("Select an option (1, 2, 3, 4, 5): "))
 
             if option == 1:
                 add_users()
@@ -125,13 +200,16 @@ def main_menu():
             
             elif option == 3:
                 search_users()
-                
+            
             elif option == 4:
+                update_users()
+                
+            elif option == 5:
                 print("\nThank You !")
                 break
                 
             else:
-                print("Invalid option. Please choose 1, 2, 3, or 4.")
+                print("Invalid option. Please choose 1, 2, 3, 4, or 5.")
         except ValueError:
             print("Please enter a valid integer.")
         except Exception as e:
